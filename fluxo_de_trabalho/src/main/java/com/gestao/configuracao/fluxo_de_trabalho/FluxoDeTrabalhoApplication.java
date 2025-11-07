9203084bb6d2dbfee4fa778b19d374df46730bbd
llkessioll <kessio_baixista@hotmail.com>
2025-11-07 14:36:38 -0300

    Inicio do projeto é primeiro commit

---
+++ fluxo_de_trabalho/src/main/java/com/gestao/configuracao/fluxo_de_trabalho/FluxoDeTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.gestao.configuracao.fluxo_de_trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoDeTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoDeTrabalhoApplication.class, args);
+	}
+
+}
---
+++ fluxo_de_trabalho/src/test/java/com/gestao/configuracao/fluxo_de_trabalho/FluxoDeTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.configuracao.fluxo_de_trabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoDeTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
