15931b49a917cf7e5a111f5f9eb0d636a5afe675
llkessioll <kessio_baixista@hotmail.com>
2025-11-12 00:38:40 -0300

    feat: adição do metodod delete e da rota (/api/livros/isbn) o metodo tambem retorna 404 caso não encontre o livro e 204 se o delete executado

--- fluxo_de_trabalho/src/main/java/com/gestao/configuracao/fluxo_de_trabalho/controller/LivroController.java
+++ fluxo_de_trabalho/src/main/java/com/gestao/configuracao/fluxo_de_trabalho/controller/LivroController.java
@@ -1,8 +1,12 @@
 package com.gestao.configuracao.fluxo_de_trabalho.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +33,18 @@ public class LivroController {
 	@PostMapping
 	public Livro addLivro(@RequestBody Livro livro) {
 		return livroRepository.save(livro);
+	}
+	
+	@DeleteMapping("/{isbn}")
+	public ResponseEntity<Void> deletarLivro(@PathVariable Long isbn){
+		Optional<Livro> livro = livroRepository.findById(isbn);
+		
+		if(livro.isEmpty()) {
+			return ResponseEntity.notFound().build();//retorna 404
+		}
+		
+		livroRepository.deleteById(isbn);
+		return ResponseEntity.noContent().build();//retorna 204
 	}
 	
 }
